8e20278d6296ca4e23cd9b20aa5094cf62252013
labadi0 <labaditarekk@gmail.com>
2021-03-04 01:30:11 +0100

    update unit test for Transfer

--- Bank_Rest/src/main/java/dao/TransferDao.java
+++ Bank_Rest/src/main/java/dao/TransferDao.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import entity.Transfer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-@Repository
+
 public interface TransferDao extends JpaRepository<Transfer, Integer> {
 	  
 	

26bf6f6bf707887c905da7d7b142b76b7b8ed4e5
labadi0 <labaditarekk@gmail.com>
2021-03-04 02:32:23 +0100

    update client unit test

--- Bank_Rest/src/test/java/servicesTest/ClientServiceTest.java
+++ Bank_Rest/src/test/java/servicesTest/ClientServiceTest.java
@@ -1,9 +1,12 @@
 package servicesTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
@@ -42,13 +45,7 @@ public class ClientServiceTest {
 		ClientService clientService = new ClientService();
 		clientService.setClientDao(clientDao);
 		Client client = new Client(1, "client1", "12AEZR1A", 200, new Transfer(1, 200));
-		/*
-		postService.saveTransfer(postRequest);
-		Mockito.verify(userRepository, Mockito.times(1)).save(postArgumentCaptor.capture());
-
-		Assertions.assertThat(postArgumentCaptor.getValue().getId()).isEqualTo(1);
-		Assertions.assertThat(postArgumentCaptor.getValue().getMoneyTransfer()).isEqualTo(200);
-		*/
+	
 		Mockito.when(clientDao.save(client)).thenReturn(client);
         Client found = clientService.saveClient(client);
 
@@ -62,14 +59,14 @@ public class ClientServiceTest {
 	@Test
 	@DisplayName("ici je teste si la methode deleteClient marche ou pas ")
 	public void deleteClientTest() {
+
 		ClientService clientService = new ClientService();
 		clientService.setClientDao(clientDao);
-		Client client = new Client(1, "client1", "12AEZR1A", 200, new Transfer(1, 200));
-		// when(userRepository.deleteById(1)).thenReturn(transferEntity);
+		Optional<Client> client = Optional.of(new Client(1, "client1", "12AEZR1A", 200, new Transfer(1, 200)));
+		Mockito.when(clientDao.findById(1)).thenReturn(client);
+		Mockito.when(clientDao.existsById(client.get().getId())).thenReturn(false);
+		assertFalse(clientDao.existsById(client.get().getId()));
 
-		clientService.deleteClient(client.getId());
-
-        Mockito.verify(clientDao, Mockito.times(1)).deleteById(client.getId());
 
 	}
 
@@ -86,6 +83,50 @@ public class ClientServiceTest {
 		assertEquals(200, tf.getAmount());
 	}
 
+	@Test
+	@DisplayName("ici je teste si la methode updateClient marche ou pas ")
+	public void updateClientTest() {
+		ClientService clientService = new ClientService();
+		clientService.setClientDao(clientDao);
+		Optional<Client> client = Optional.of(new Client(1, "client1", "12AEZR1A", 200, new Transfer(1, 200)));
+		Mockito.when(clientDao.findById(1)).thenReturn(client);
+		client.get().setAmount(100);
+		Mockito.when(clientDao.save(client.get())).thenReturn(client.get());
+		assertThat(clientService.updateClientById(client.get()).getAmount()).isEqualTo(client.get().getAmount());
+		assertThat(client.get().getId()).isEqualTo(1);
+
+		// System.out.println(transfer.get().getMoneyTransfer());
+	}
+
+	@Test
+	@DisplayName("ici je teste si la methode getAllClient marche ou pas ")
+	public void getAllClientTest() {
+		ClientService clientService = new ClientService();
+		clientService.setClientDao(clientDao);
+		Client client = new Client(1, "client1", "12AEZR1A", 200, new Transfer(1, 200));
+		Client client2 = new Client(1, "client2", "12AEZdsqdqR1A", 210, new Transfer(2, 220));
+		ArrayList<Client> clientsList = new ArrayList<>();
+		clientsList.add(client);
+		clientsList.add(client2);
+		Mockito.when(clientDao.findAll()).thenReturn(clientsList);
+		assertThat(clientService.getAllClient().size()).isEqualTo(clientsList.size());
+	}
+	
+	
+	@Test
+	@DisplayName("ici je teste si la methode saveAllClient marche ou pas ")
+	public void saveAllClientTest() {
+		ClientService clientService = new ClientService();
+		clientService.setClientDao(clientDao);
+		Client client = new Client(1, "client1", "12AEZR1A", 200, new Transfer(1, 200));
+		Client client2 = new Client(1, "client2", "12AEZdsqdqR1A", 210, new Transfer(2, 220));
+		ArrayList<Client> clientList = new ArrayList<>();
+		clientList.add(client);
+		clientList.add(client2);		
+		Mockito.when(clientDao.saveAll(clientList)).thenReturn(clientList);
+		assertThat(clientService.saveAllClients(clientList).size()).isEqualTo(clientList.size());
+	}
+	
 	
 	
 
